b52f3185e6226bba93c5d4d9f81ce5ec77c377d3
Luis Chavez <lchavezpozo@gmail.com>
2021-04-03 13:11:17 -0500

    solved fixes in AcronymListView

--- Acronyms/Modules/AcronymSearchView/Views/AcronymListView/AcronymListView.swift
+++ Acronyms/Modules/AcronymSearchView/Views/AcronymListView/AcronymListView.swift
@@ -13,9 +13,9 @@ protocol AcronymListViewDelegate: class {
 }
 
 class AcronymListView: UIView, EmptyViewProtocol {
-
+    
     weak var delegate: AcronymListViewDelegate?
-
+    
     private lazy var tableView: UITableView = {
         let tv = UITableView()
         tv.translatesAutoresizingMaskIntoConstraints = false
@@ -26,21 +26,21 @@ class AcronymListView: UIView, EmptyViewProtocol {
         tv.dataSource = self
         return tv
     }()
-
+    
     lazy var emptyView: SearchEmptyView = {
         let view = SearchEmptyView(title: "Sin Resultados")
         return view
     }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func reloadList() {
         tableView.reloadData()
     }
@@ -48,33 +48,32 @@ class AcronymListView: UIView, EmptyViewProtocol {
 }
 
 extension AcronymListView: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return delegate?.acronymListViewNumberOfRowsInSection() ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier:  AcronymCell.identifier) as? AcronymCell
-        guard let cellViewModel =  delegate?.acronymListView(cellViewModelFor: indexPath.row) else { return UITableViewCell() }
-        cell?.configureCell(with: cellViewModel)
-        return cell ?? UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier:  AcronymCell.identifier) as? AcronymCell,
+              let cellViewModel =  delegate?.acronymListView(cellViewModelFor: indexPath.row) else { return UITableViewCell() }
+        cell.configureCell(with: cellViewModel)
+        return cell
     }
 
 }
 
 private extension AcronymListView {
-
+    
     func setupUI() {
         setupTableView()
     }
     
     func setupTableView() {
         addSubview(tableView)
-        addSubview(tableView)
         tableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
-
+    
 }
